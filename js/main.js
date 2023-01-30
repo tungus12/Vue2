5e8295ffefe3cc05f309f6f5de2822933a2f1c2a
tungus12 <012nikita_012@mail.ru>
2023-01-30 11:54:50 +0700

    practos 2 endftf

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-let product = "Socks";
-
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        description: 'A pair of warm fuzzy socks',
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
 })
