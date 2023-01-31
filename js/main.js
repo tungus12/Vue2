ea4e007f07d759cb9313a3091cfd08c5a1a5313a
nikita <012nikita_012@mail.ru>
2023-01-31 23:20:37 +0700

    практ-7

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: 'A pair of warm fuzzy socks',
-        image: "./img/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: false,
@@ -14,12 +15,14 @@ let app = new Vue({
             {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './img/vmSocks-green-onWhite.jpg'
+        variantImage: './img/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
       },
              {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './img/vmSocks-blue-onWhite.jpg'
+        variantImage: './img/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0
       },
 
         ],
@@ -27,17 +30,40 @@ let app = new Vue({
         cart: 0,
 
 
+
+
     },
      methods: {
          addToCart() {
              this.cart += 1
          },
-         updateProduct(variantImage) {
-             this.image = variantImage
-         },
+         updateProduct(index) {
+   this.selectedVariant = index;
+   console.log(index);
+},
+
          removeFromCart() {
              this.cart -= 1
          }
-     }
+     },
+        computed: {
+   title() {
+       return this.brand + ' ' + this.product;
+   },
+   image() {
+   return this.variants[this.selectedVariant].variantImage;
+},
+    inStock(){
+   return this.variants[this.selectedVariant].variantQuantity
+},
+    sale() {
+          if (this.onSale) {
+            return this.brand + ' ' + this.product + ' are on sale!'
+          }
+            return  this.brand + ' ' + this.product + ' are not on sale'
+        }
+}
+
+
 })
 
