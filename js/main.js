6573fadecfcf5648c0d5b3c2d6c9f1e07599671a
nikita <012nikita_012@mail.ru>
2023-01-31 17:41:02 +0700

    практ1-5

--- js/main.js
+++ js/main.js
@@ -3,8 +3,41 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: 'A pair of warm fuzzy socks',
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./img/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
-    }
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './img/vmSocks-green-onWhite.jpg'
+      },
+             {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './img/vmSocks-blue-onWhite.jpg'
+      },
+
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+
+    },
+     methods: {
+         addToCart() {
+             this.cart += 1
+         },
+         updateProduct(variantImage) {
+             this.image = variantImage
+         },
+         removeFromCart() {
+             this.cart -= 1
+         }
+     }
 })
+
